2e72796ca77c088337e6e8461d397a595734ce70
karan.ahuja.claude <karan.ahuja.claude@gmail.com>
2026-09-08 20:14:56 +0530

    Connect Supabase PostgreSQL, fetch schema via query, and integrate with main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+from mydb import execute_query, get_schema
 
 load_dotenv()  # Load environment variables from .env file
+
 
 '''
 my_key=os.getenv('OPENAI_API_KEY')
@@ -11,6 +13,17 @@ print(f"my_key: {my_key}")'''
 
 client=OpenAI()
 
-responses=client.responses.create(model='gpt-5.6-sol',input="tell me about genai in 20 words")
+schema=get_schema("order")
 
-print(responses.output_text)
+while True:
+    user_input = input("Ask away: ")
+    if user_input.lower() in ['exit', 'quit']:
+        print("Exiting the program.")
+        break    
+
+    question=user_input   
+    final_prompt=f"""Generate a postgresql query based on this {schema} and this question: {question}"""
+    responses=client.responses.create(model='gpt-5.6-sol',input=final_prompt)
+
+    print(responses.output_text)
+    print()
---
+++ mydb.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine, text
+import pandas as pd
+import psycopg2
+
+# Replace with your actual database details
+DB_HOST = "aws-0-ap-northeast-2.pooler.supabase.com"
+DB_NAME = "postgres"
+DB_USER = "postgres.nrgkpbjgqwwvejwqyojo"
+DB_PASSWORD = "oWrWR7gituTcBi46"
+DB_PORT = "6543"
+
+# SQLAlchemy connection string format
+engine = create_engine(
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+def execute_query(query):
+    with engine.connect() as conn:
+        result = conn.execute(query)
+        df = pd.DataFrame(result.fetchall(), columns=result.keys())
+    return df
+
+def get_schema(table_name):
+        query = text(f"""
+            SELECT table_name, column_name, data_type
+            FROM information_schema.columns
+            WHERE table_schema = 'public' AND table_name = '{table_name}'
+            ORDER BY ordinal_position;
+        """)
+        schema = execute_query(query)
+        return schema
+
+result = get_schema("order")
+print(result)
